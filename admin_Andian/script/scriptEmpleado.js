34776ea49826ff6065afb7912a05429db4db7251
cristyags <20235929@esen.edu.sv>
2024-11-14 19:48:27 -0600

    Log In
    Hay log in
    boton de log out
    validacion entre empleado y admin

--- admin_Andian/script/scriptEmpleado.js
+++ admin_Andian/script/scriptEmpleado.js
@@ -1,1 +1,14 @@
 import { saveEmpleado, getEmpleados, getEmpleado, getEmpleadoListSize, deleteEmpleado, updateEmpleado } from "./firebase.js";
+
+if(localStorage.getItem("userRole") && localStorage.getItem("userRole") === 'empleado'){
+    const navItem = document.getElementById('nav-item-employee');
+    navItem.style.display = 'none'
+    const dashboardEmployee = document.getElementById('dashboard-employee');
+    dashboardEmployee.style.display = 'none'
+}
+
+const logoutBtn = document.querySelector('#logout-btn');
+logoutBtn.addEventListener('click', () => {
+    localStorage.clear();
+    window.location.href = 'login.html'
+})
--- admin_Andian/script/scriptLogin.js
+++ admin_Andian/script/scriptLogin.js
@@ -1,1 +1,30 @@
-import { saveEmpleado, getEmpleados, getEmpleado, getEmpleadoListSize, deleteEmpleado, updateEmpleado } from "./firebase.js";
+// import { saveEmpleado, getEmpleados, getEmpleado, getEmpleadoListSize, deleteEmpleado, updateEmpleado } from "./firebase.js";
+
+const formLogin = document.querySelector('#loginForm');
+formLogin.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const users = {
+        admin: { password: 'admin123', role: 'admin' },
+        empleado1: { password: 'empleado123', role: 'empleado' },
+    };
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const message = document.getElementById('message');
+
+    message.textContent = "";
+
+    if (users[username] && users[username].password === password) {
+        const role = users[username].role;
+
+        // Guarda el rol y el nombre de usuario en localStorage
+        localStorage.setItem("userRole", role);
+        localStorage.setItem("username", username);
+
+        // Redirige a la página de inicio (index.html)
+        window.location.href = "index.html";
+    } else {
+        message.textContent = 'Usuario o contraseña incorrectos.';
+        message.style.color = 'red';
+    }
+})
